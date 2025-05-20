dd359a1b0eb0ada2d7b8f8009ae4074ed651620b
Robson777555 <136523255+Robson777555@users.noreply.github.com>
2025-05-20 18:00:42 -0300

    Update metadata.js

--- src/app/metadata.js
+++ src/app/metadata.js
@@ -2,7 +2,7 @@ export const metadata = {
   title: 'Landing Page Next.js',
   description: 'Uma landing page moderna construída com Next.js',
   keywords: ['Next.js', 'React', 'Landing Page'],
-  authors: [{ name: 'Seu Nome' }],
+  authors: [{ name: 'Robson Santos Jobim' }],
   viewport: 'width=device-width, initial-scale=1',
   robots: 'index, follow',
   openGraph: {
